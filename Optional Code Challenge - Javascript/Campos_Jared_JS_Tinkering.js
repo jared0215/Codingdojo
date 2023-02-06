6ec47a7796d86ccc47a0700f9031c7613609d8c3
jared0215 <jared0215@hotmail.com>
2023-02-06 17:55:38 -0500

    I have no idea what I jsut did

---
+++ Optional Code Challenge - Javascript/Campos_Jared_JS_Tinkering.js
@@ -0,0 +1,10 @@
+var childHeight = 70;
+function displayIfChildIsAbleToRideTheRollerCoaster(childHeight) {
+    if (childHeight > 52) {
+        console.log("Get on that ride, kiddo!");
+    }
+    else {
+        console.log("Sorry kiddo. Maybe next year.");
+    }
+}
+return displayIfChildIsAbleToRideTheRollerCoaster(childHeight);
