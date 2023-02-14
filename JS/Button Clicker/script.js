e79136ff053cbabe7a7d88a61212fc8cf3edb7af
Jared Campos <99213397+jared0215@users.noreply.github.com>
2023-02-14 17:02:32 -0500

    Video Preview Finished

---
+++ JS/Button Clicker/script.js
@@ -0,0 +1,9 @@
+function logOut(element) {
+    element.innerText = "Logout";
+}
+function hideBtn(element) {
+    element.remove();
+}
+function alert(ninja) {
+    console.log("Ninja was liked", ninja);
+}
