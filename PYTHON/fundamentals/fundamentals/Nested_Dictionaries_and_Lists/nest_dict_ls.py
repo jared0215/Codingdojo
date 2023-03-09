51f306b15f387f07ccbf8095549897f0e146e047
Jared Campos <99213397+jared0215@users.noreply.github.com>
2023-03-09 18:19:06 -0500

    Finished Second Quetion

--- PYTHON/fundamentals/fundamentals/Nested_Dictionaries_and_Lists/nest_dict_ls.py
+++ PYTHON/fundamentals/fundamentals/Nested_Dictionaries_and_Lists/nest_dict_ls.py
@@ -35,10 +35,14 @@ students = [
     {'first_name': 'Mark', 'last_name': 'Guillen'},
     {'first_name': 'KB', 'last_name': 'Tonel'}
 ]
+
+
+def iterateDictionary(students):
+    for x in students:
+        print('first_name', "-", x['first_name'] +
+              ",", 'last_name', "-", x['last_name'])
+
+
 iterateDictionary(students)
-# should output: (it's okay if each key-value pair ends up on 2 separate lines;
-# bonus to get them to appear exactly as below!)
-first_name - Michael, last_name - Jordan
-first_name - John, last_name - Rosales
-first_name - Mark, last_name - Guillen
-first_name - KB, last_name - Tonel
+
+#
