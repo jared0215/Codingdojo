d73f41877ecef136e5bdf1370ad64473112b5ae4
Jared Campos <99213397+jared0215@users.noreply.github.com>
2023-03-09 19:18:08 -0500

    Finished

--- PYTHON/fundamentals/fundamentals/Nested_Dictionaries_and_Lists/nest_dict_ls.py
+++ PYTHON/fundamentals/fundamentals/Nested_Dictionaries_and_Lists/nest_dict_ls.py
@@ -1,5 +1,5 @@
 
-# TODO: Update Values in Dictionaries and Lists
+# TODO: 1 - Update Values in Dictionaries and Lists
 x = [[5, 2, 3], [10, 8, 9]]
 students = [
     {'first_name':  'Michael', 'last_name': 'Jordan'},
@@ -11,24 +11,24 @@ sports_directory = {
 }
 z = [{'x': 10, 'y': 20}]
 
-# 1 - Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
+# 1.1 - Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
 x[1][0] = 15
 print(x)
 
-# 2 - Change the last_name of the first student from 'Jordan' to 'Bryant'
+# 1.2 - Change the last_name of the first student from 'Jordan' to 'Bryant'
 students[0]['last_name'] = 'Bryant'
 print(students[0]["last_name"])
 
-# 3 - In the sports_directory, change 'Messi' to 'Andres'
+# 1.3 - In the sports_directory, change 'Messi' to 'Andres'
 sports_directory['soccer'][0] = 'Andres'
 print(sports_directory['soccer'][0])
 print(sports_directory)
 
-# 4 - Change the value 20 in z to 30
+# 1.4 - Change the value 20 in z to 30
 z[0]['y'] = 30
 print(z)
 
-# TODO: Iterate Through a List of Dictionaries
+# TODO: 2 - Iterate Through a List of Dictionaries
 students = [
     {'first_name':  'Michael', 'last_name': 'Jordan'},
     {'first_name': 'John', 'last_name': 'Rosales'},
@@ -45,4 +45,29 @@ def iterateDictionary(students):
 
 iterateDictionary(students)
 
-#
+# TODO: 3 - Get Values From a List of Dictionaries
+
+
+def iterateDictionary2(key_name, some_list):
+    for x in some_list:
+        print(x[key_name])
+
+
+iterateDictionary2('last_name', students)
+
+# TODO: 4 - Iterate Through a Dictionary with List Values
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+
+def printInfo(some_dict):
+    for key in some_dict.keys():
+        print(len(some_dict[key]), key.upper())
+        for val in some_dict[key]:
+            print(val.upper())
+        print("")
+
+
+printInfo(dojo)
