d1efe67793d3145b7272b908cfdee1d73b826adf
Jared Campos <99213397+jared0215@users.noreply.github.com>
2023-03-25 18:17:34 -0400

    Finished playground

--- PYTHON/flask/fundamentals/playground/server.py
+++ PYTHON/flask/fundamentals/playground/server.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/play', defaults={'times': 3})
-@app.route('/play/<int:times>')
-def index(times):
-    return render_template('index.html', times=times)
+@app.route('/play', defaults={'times': 3, 'color': 'lightblue'})
+@app.route('/play/<int:times>', defaults={'color': 'lightblue'})
+@app.route('/play/<int:times>/<string:color>')
+@app.route('/play/<string:color>', defaults={'times': 3})
+def index(times, color):
+    return render_template('index.html', times=times, box_color=color)
 
 
 if __name__ == '__main__':
