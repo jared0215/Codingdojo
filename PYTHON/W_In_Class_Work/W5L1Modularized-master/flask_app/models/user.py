2f00d993a32a5bc3d4aeb07d0c10e5994bf90d71
jared0215 <jared0215@hotmail.com>
2023-04-06 14:34:41 -0400

    Oops

---
+++ PYTHON/W_In_Class_Work/W5L1Modularized-master/flask_app/models/user.py
@@ -0,0 +1,95 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+
+from flask_app.models import comment
+
+class User:
+
+    db="facegram"
+
+    def __init__(self,data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name=data['last_name']
+        self.email=data['email']
+        self.password=data['password']
+        self.created_at=data['created_at']
+        self.updated_at=data['updated_at']
+
+
+
+    #CREATE
+    @classmethod
+    def save(cls,data):
+        
+        query="""
+        INSERT INTO users(first_name,last_name,email,password) 
+        VALUES(%(first_name)s,%(last_name)s,%(email)s,%(password)s);
+        """
+        results = connectToMySQL(cls.db).query_db(query,data)
+        return results
+    
+    #READ
+    @classmethod
+    def get_all(cls):
+        query="SELECT * FROM users;"
+        results = connectToMySQL(cls.db).query_db(query)
+        print(results)
+        users =[]
+        for row in results:
+            users.append(cls(row))
+        return users
+    
+    @classmethod
+    def get_one_by_email(cls,email):
+        data={
+            "email":email
+        }
+        query="""
+        SELECT * FROM users WHERE email=%(email)s;
+        """
+        results = connectToMySQL(cls.db).query_db(query,data)
+        if not results:
+            return []
+        return cls(results[0])
+    
+    @classmethod
+    def get_one(cls,id):
+        data={
+            "id":id
+        }
+        query="""
+        SELECT * FROM users WHERE id=%(id)s;
+        """
+        results = connectToMySQL(cls.db).query_db(query,data)
+        return cls(results[0])
+
+    #UPDATE
+    @classmethod
+    def update(cls, data):
+        query = """
+        UPDATE users 
+        SET first_name = %(first_name)s,
+        last_name = %(last_name)s,
+        email = %(email)s,
+        password = %(password)s 
+        WHERE id = %(id)s;"""
+
+        results = connectToMySQL(cls.db).query_db(query, data)
+        return results
+
+
+    #DELETE
+    @classmethod
+    def delete(cls,id):
+        data={
+            'id':id
+        }
+        query="""
+        DELETE FROM users
+        WHERE id=%(id)s;
+        """
+        results = connectToMySQL(cls.db).query_db(query, data)
+        return results
+        
+
+
