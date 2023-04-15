4b26c204ab5606b022ef806a1d5c1f947eb45e91
Jared Campos <99213397+jared0215@users.noreply.github.com>
2023-04-15 18:59:51 -0400

    Mistake

--- Black Belt Exam/flask_app/static/js/script.js
+++
@@ -1,8 +0,0 @@
-function togglePassword() {
-        var passwordInput = document.getElementsByName("password")[0];
-        if (passwordInput.type === "password") {
-            passwordInput.type = "text";
-        } else {
-            passwordInput.type = "password";
-        }
-    }
